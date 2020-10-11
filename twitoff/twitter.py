1f22cac30dbd421f7479adc00324995c836bed1f
jeremy-jewett <64876522+jeremy-jewett@users.noreply.github.com>
2020-10-10 20:13:20 -0400

    Found a missing colon that was messing everything up.

--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -138,6 +138,6 @@ def update_all_users():
 
 def update_all_users():
         '''This updates all the users'''
-    for user in User.query.all()
+    for user in User.query.all():
         add_user_tweepy(user.username)
 
