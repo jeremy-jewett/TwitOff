fe7cf5bc15765f0d31b98aae4823d1948f0df377
jeremy-jewett <64876522+jeremy-jewett@users.noreply.github.com>
2020-10-10 20:51:48 -0400

    added s to another user

--- twitoff/app.py
+++ twitoff/app.py
@@ -51,7 +51,7 @@ def create_app():
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title='Reset Database!', user=User.query.all())
+        return render_template('base.html', title='Reset Database!', users=User.query.all())
 
     @app.route('/update', methods=['GET'])
     def update():
