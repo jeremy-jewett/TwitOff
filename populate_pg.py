5393f5a35c65fc1deaaa6de192e1b863cbc2f2b4
jeremy-jewett <64876522+jeremy-jewett@users.noreply.github.com>
2020-10-10 20:48:19 -0400

    created populate file

---
+++ populate_pg.py
@@ -0,0 +1,20 @@
+'''Populate TwitOff with users'''
+import requests
+
+URL = 'https://dspt7-twitoff.herokuapp.com/user'
+
+USERNAMES = [
+    'BarackObama','justinbieber','katyperry','rihanna','taylorswift13',
+    'Cristiano','ladygaga','TheEllenShow','realDonaldTrump','YouTube'
+]
+
+def populate_twitoff(url, usernames):
+'''POST to url for each name in usernames'''
+    with requests.Session() as session:
+        for username in usernames:
+            print(username)
+            r = session.post(url, data={'user_name': username})
+
+if __name__ == '__main__':
+
+populate_twitoff(URL, USERNAMES)
