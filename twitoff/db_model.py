201c81e46976185a10b83acb3f5e107099623566
jeremy-jewett <64876522+jeremy-jewett@users.noreply.github.com>
2020-10-10 22:26:15 -0400

    reverted order in user_id back

--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -16,7 +16,7 @@ class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
     tweet = DB.Column(DB.String(500), unique=True, nullable=False)
     embedding = DB.Column(DB.PickleType, nullable=False)
-    user_id = DB.Column(DB.BigInteger, nullable=False, DB.ForeignKey('user.id'))
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweet', lazy=True))
 
     def __repr__(self):
